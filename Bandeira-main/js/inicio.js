d8c19ea898561aa80d305eb22bbe4a5901003d44
Lívia Ozório <livia.ozorio@aluno.senai.br>
2025-06-08 21:21:07 -0300

    Update inicio.js

--- Bandeira-main/js/inicio.js
+++ Bandeira-main/js/inicio.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function () {
     var botaoJogar = document.getElementById("jogarBnt");
     if (botaoJogar) {
         botaoJogar.addEventListener("click", function () {
-            window.location.href = "html/login.html";
+            window.location.href = "html/login2.html";
         });
     }
 });
